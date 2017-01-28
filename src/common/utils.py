b489a0465926673322a23657a8002b6f4bacfc9b
Mihai <mihai32@gmail.com>
2017-01-28 12:10:29 +0200

    Finalize database.py, create utils.py, create errors.py, work to users.py

---
+++ src/common/utils.py
@@ -0,0 +1,23 @@
+from passlib.hash import pbkdf2_sha512
+
+class Utils(object):
+
+    @staticmethod
+    def hash_password(password):
+        """
+        Hashes a password using pbkdf2_sha512
+        :param password: the sha512 from the login/register form
+        :return: A sha512 -> pbkdf2_sha512 encrypted password
+        """
+        return pbkdf2_sha512.encrypt(password)
+
+    @staticmethod
+    def checked_hashed_password(password, hashed_password):
+        """
+        Check that the password that the user sent match the password in the database.
+        The database password is encrypted more than the user's password at this stage.
+        :param password: shat512-hashed password
+        :param encrypted_hashed_password: pbkdf2-shat512 encrypted password
+        :return: True if password matches, False otherwise
+        """
+        return pbkdf2_sha512.verify(password, hashed_password)
--- src/models/users/user.py
+++ src/models/users/user.py
@@ -1,11 +1,37 @@
+import uuid
+
+from src.common.database import Database
+from src.common.utils import Utils
+# import both classes from src.models.users.errors and give a name UserErrors
+import src.models.users.errors as UserErrors
 
 
 class User(object):
 
-    def __init__(self, email, password):
+    def __init__(self, email, password, _id=None):
         self.email = email
         self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     # string representation
     def __repr__(self):
         return "<User {}".format(self.email)
+
+
+    @staticmethod
+    def is_login_valid(email, password):
+        """
+        This method verifies in the database if the email, password pair is valid or note.
+        Check if email exists, and if the password associated with the email is valid.
+        :param email: The user's email
+        :param password: A sha512 hashed password
+        :return: True if valid, False otherwise
+        """
+        # password in sha512 -> pbkdf2_sha512
+        user_data = Database.find_one(collection='users', query={"email": email})
+        if user_data is None:
+            # tell the user that he email provided is not found in the database
+            raise UserErrors.UserNotExistsError('Your user does not exist.')
+        if not Utils.checked_hashed_password(password, user_data['password']):
+            # tell the user that the password provided is wrong
+            raise UserErrors.IncorrectPasswordError('The password provided was wrong.')
