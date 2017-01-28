fe3bb82c80d8c0c67e6cd11c3e727633d4077020
Mihai <mihai32@gmail.com>
2017-01-28 17:39:44 +0200

    In users/views.py - define login_user(), in src/app.py - register the user_blueprint

--- src/app.py
+++ src/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from src.common.database import Database
 
 app = Flask(__name__)
 # load config file
 app.config.from_object('config')
 
-@app.route('/')
-def hi_world():
-    return 'Hi, world'
+
+#initialize database
+@app.before_first_request
+def init_db():
+    Database.initialize()
+
+
+# register the user_blueprint
+from src.models.users.views import user_blueprint
+app.register_blueprint(user_blueprint, url_prefix='/users')
--- src/models/users/views.py
+++ src/models/users/views.py
@@ -1,14 +1,29 @@
 from flask import Blueprint
+from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
+from werkzeug.utils import redirect
 
+from src.models.users.user import User
 
 # define the user_blueprint
 user_blueprint = Blueprint('users', __name__)
 
 
 # define login end-point
-@user_blueprint.route('/login')
+@user_blueprint.route('/login', methods=['GET', 'POST'])
 def login_user():
-    pass
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['hashed']
+
+        # check if the email, password are valid
+        if User.is_login_valid(email, password):
+            session['email'] = email
+            # go to the def user_alerts()
+            return redirect(url_for(".user_alerts"))
+    return render_template("users/login.html")
 
 
 # define logout end-point
